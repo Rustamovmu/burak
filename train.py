973db36388946b2fa3a995042bb5db7033d00b67
M.U <rustamovmuhammadumar2004@gmail.com>
2026-05-26 00:09:07 +0900

    fix: fixed

--- train.py
+++ train.py
@@ -5,14 +5,42 @@
 # Masalan: reverseSentence("we like coding!") return "ew ekil !gnidoc"
 
 
-def reverseSentence(sentence):
-    words = sentence.split()
-    reverse_words = []
+# def reverseSentence(sentence):
+#     words = sentence.split()
+#     reverse_words = []
 
-    for word in words:
-        reverse_words.append(word[::-1])
+#     for word in words:
+#         reverse_words.append(word[::-1])
 
-    return " ".join(reverse_words)
+#     return " ".join(reverse_words)
 
 
-print(reverseSentence("we like coding!"))
+# print(reverseSentence("we like coding!"))
+
+
+# script.jsEditor
+# Console
+# Build a Celsius to Fahrenheit Converter
+# In this lab, you will write a function that converts the temperature from Celsius to Fahrenheit. The formula to convert from Celsius to Fahrenheit is:
+
+# Example Code
+# fahrenheit = celsius * (9/5) + 32
+# Objective: Fulfill the user stories below and get all the tests to pass to complete the lab.
+
+# User Stories:
+
+# You should create a function named convertCtoF.
+# convertCtoF should take a single numeric argument, which is the temperature in Celsius.
+# convertCtoF should return a number.
+# Tests:
+# Waiting:1. You should create a function named convertCtoF.
+# Waiting:2. convertCtoF should take a single parameter.
+# Waiting:3. convertCtoF(0) should return a number.
+
+
+# def convertCtoF(celsius):
+#     fahrenheit = celsius * (9/5) + 32
+#     return fahrenheit
+
+
+# print(convertCtoF(0))
