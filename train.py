9ad0bfcda85e4519a3a65f96c28b36740874be51
M.U <rustamovmuhammadumar2004@gmail.com>
2026-05-26 14:28:54 +0900

    feat: MIT task M has been done

--- train.py
+++ train.py
@@ -1,3 +1,22 @@
+# TASK M
+
+# Array ichidagi har bir raqam uchun raqamning o'zi va
+# uning kvadratidan tashkil topgan object hosil qilib qaytarsin.
+
+# Masalan: getSquareNumbers([1, 2, 3]) return [{number: 1, square: 1}, ...]
+def getSquareNumbers(arr):
+    result = []
+    for num in arr:
+        obj = {
+            "number": num,
+            "square": num ** 2
+        }
+    result.append(obj)
+    return result
+
+
+print(getSquareNumbers([1, 2, 3]))
+
 # TASK L
 
 # So'zlarni ketma-ketligini buzmasdan har bir so'zni alohida teskarisiga o'girib beradigan function tuzing.
