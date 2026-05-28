5bffb0cb9266937bc43d027eef3fc3b2c7b8ee44
M.U <rustamovmuhammadumar2004@gmail.com>
2026-05-29 00:23:47 +0900

    feat: creating routing and member controller

--- train.py
+++ train.py
@@ -4,18 +4,18 @@
 # uning kvadratidan tashkil topgan object hosil qilib qaytarsin.
 
 # Masalan: getSquareNumbers([1, 2, 3]) return [{number: 1, square: 1}, ...]
-def getSquareNumbers(arr):
-    result = []
-    for num in arr:
-        obj = {
-            "number": num,
-            "square": num ** 2
-        }
-    result.append(obj)
-    return result
+# def getSquareNumbers(arr):
+#     result = []
+#     for num in arr:
+#         obj = {
+#             "number": num,
+#             "square": num ** 2
+#         }
+#     result.append(obj)
+#     return result
 
 
-print(getSquareNumbers([1, 2, 3]))
+# print(getSquareNumbers([1, 2, 3]))
 
 # TASK L
 
