adea38d3859a551bc86409159d8c68f3378b708f
M.U <rustamovmuhammadumar2004@gmail.com>
2026-06-01 11:42:48 +0900

    feat: create restaurant member related router get and post endpoints

--- src/routerAdmin.ts
+++ src/routerAdmin.ts
@@ -1,12 +1,16 @@
 import express from "express";
 const routerAdmin = express.Router();
 import restaurantController from "./controllers/restaurant.controller";
-
+/** Restaurant Routes */
 routerAdmin.get("/", restaurantController.goHome);
+routerAdmin
+    .get("/login", restaurantController.getLogin)
+    .post("/login", restaurantController.processLogin);
+routerAdmin
+    .get("/signup", restaurantController.getSignup)
+    .post("/signup", restaurantController.processSignup);
 
-routerAdmin.get("/login", restaurantController.getLogin);
-
-routerAdmin.get("/signup", restaurantController.getSignup);
-
+/** Product Routes */
+/** User Routes */
 
 export default routerAdmin;
