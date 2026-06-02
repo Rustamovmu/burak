490797341318976a5e649ffa3f2802726a69b2f2
M.U <rustamovmuhammadumar2004@gmail.com>
2026-06-02 13:52:54 +0900

    feat: develop signup process

---
+++ src/libs/types/member.ts
@@ -0,0 +1,28 @@
+import { MemberStatus, MemberType } from "../enums/member.enum";
+import { ObjectId } from "mongoose";
+export interface Member {
+    _id: ObjectId;
+    memberType: MemberType;
+    memberStatus: MemberStatus;
+    memberNick: string;
+    memberPhone: string;
+    memberPassword?: string;
+    memberImage?: string;
+    memberAddress?: string;
+    memberDesc?: string;
+    memberPoints: number;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+export interface MemberInput {
+    memberType?: MemberType;
+    memberStatus?: MemberStatus;
+    memberNick: string;
+    memberPhone: string;
+    memberPassword: string;
+    memberImage?: string;
+    memberAddress?: string;
+    memberDesc?: string;
+    memberPoints?: number;
+}
