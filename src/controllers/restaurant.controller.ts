3fda12be407cbb9a7c3e4b859725c68f360a150c
M.U <rustamovmuhammadumar2004@gmail.com>
2026-06-08 07:23:47 +0900

    feat: develop logout process

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -3,7 +3,7 @@ import { T } from "../libs/types/common";
 import MemberService from "../models/Member.service";
 import { AdminRequest, LoginInput, MemberInput } from "../libs/types/member";
 import { MemberType } from "../libs/enums/member.enum";
-import { Message } from "../libs/Errors";
+import Errors, { Message } from "../libs/Errors";
 
 const memberService = new MemberService();
 const restaurantController: T = {};
@@ -14,17 +14,7 @@ restaurantController.goHome = (req: Request, res: Response) => {
 
     } catch (err) {
         console.log("Error, goHome:", err);
-    }
-
-};
-
-restaurantController.getLogin = (req: Request, res: Response) => {
-    try {
-        console.log("getLogin");
-        res.render("login");
-
-    } catch (err) {
-        console.log("Error, getLogin:", err);
+        res.redirect("/admin");
     }
 
 };
@@ -36,11 +26,22 @@ restaurantController.getSignup = (req: Request, res: Response) => {
 
     } catch (err) {
         console.log("Error, getSignup:", err);
+        res.redirect("/admin");
     }
 
 };
 
+restaurantController.getLogin = (req: Request, res: Response) => {
+    try {
+        console.log("getLogin");
+        res.render("login");
 
+    } catch (err) {
+        console.log("Error, getLogin:", err);
+        res.redirect("/admin");
+    }
+
+};
 
 restaurantController.processSignup = async (req: AdminRequest, res: Response) => {
     try {
@@ -57,7 +58,11 @@ restaurantController.processSignup = async (req: AdminRequest, res: Response) =>
         });
     } catch (err) {
         console.log("Error, processSignup:", err);
-        res.send(err);
+        const message =
+            err instanceof Errors ? err.message : Message.SOMETHING_WENT_WRONG;
+        res.send(
+            `<script>alert('${message}'); window.location.replace('/admin/signup');</script>`
+        );
     }
 
 };
@@ -77,7 +82,25 @@ restaurantController.processLogin = async (req: AdminRequest, res: Response) => 
         });
     } catch (err) {
         console.log("Error, processLogin:", err);
-        res.send(err);
+        const message =
+            err instanceof Errors ? err.message : Message.SOMETHING_WENT_WRONG;
+        res.send(
+            `<script>alert('${message}'); window.location.replace('/admin/login');</script>`
+        );
+
+    }
+
+};
+
+restaurantController.logout = async (req: AdminRequest, res: Response) => {
+    try {
+        console.log("logout");
+        req.session.destroy(function () {
+            res.redirect("/admin");
+        });
+    } catch (err) {
+        console.log("Error, logout:", err);
+        res.redirect("/admin");
     }
 
 };
