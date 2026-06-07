30ce7fa53f3f3c676ffb62f71d1159ad575ccce3
M.U <rustamovmuhammadumar2004@gmail.com>
2026-06-08 06:54:21 +0900

    feat: create checkAuthSession api

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -3,6 +3,7 @@ import { T } from "../libs/types/common";
 import MemberService from "../models/Member.service";
 import { AdminRequest, LoginInput, MemberInput } from "../libs/types/member";
 import { MemberType } from "../libs/enums/member.enum";
+import { Message } from "../libs/Errors";
 
 const memberService = new MemberService();
 const restaurantController: T = {};
@@ -81,4 +82,19 @@ restaurantController.processLogin = async (req: AdminRequest, res: Response) => 
 
 };
 
+restaurantController.checkAuthSession = async (req: AdminRequest, res: Response) => {
+    try {
+        console.log("checkAuthSession");
+        if (req.session?.member) {
+            res.send(`<script>alert('Hi ${req.session.member.memberNick}, you are authenticated!')</script>`);
+        } else {
+            res.send(`<script>alert('${Message.NOT_AUTHORIZED}')</script>`);
+        }
+
+    } catch (err) {
+        console.log("Error, checkAuthSession:", err);
+        res.send(err);
+    }
+
+};
 export default restaurantController;
