44a070e2fce66b7e90e029de9db1c36a19c2cada
M.U <rustamovmuhammadumar2004@gmail.com>
2026-06-16 13:16:18 +0900

    feat; modify updateChosenProduct business logic

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -55,6 +55,12 @@ productController.createNewProduct = async (req: AdminRequest, res: Response) =>
 productController.updateChosenProduct = async (req: Request, res: Response) => {
     try {
         console.log("updateChosenProduct");
+        const id = req.params.id;
+        console.log("id:", id);
+
+        const result = await productService.updateChosenProduct(id, req.body)
+
+        res.status(HttpCode.OK).json({ data: result });
 
     } catch (err) {
         console.log("Error, updateChosenProduct:", err);
