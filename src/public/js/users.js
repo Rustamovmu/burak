77ce2279abf762b37c6df8b7355d7d746fb78596
M.U <rustamovmuhammadumar2004@gmail.com>
2026-06-23 17:53:32 +0900

    fix: admin project refactoring

--- src/public/js/users.js
+++ src/public/js/users.js
@@ -3,11 +3,8 @@ console.log("Users frontend javascript file");
 
 $(function () {
     $(".member-status").on("change", function (e) {
-        const id = e.target.id;
-        console.log("id:", id);
-
-        const memberStatus = $(`#${id}.member-status`).val();
-        console.log("memberStatus:", memberStatus);
+        const id = e.target.id,
+            memberStatus = $(`#${id}.member-status`).val();
 
         axios
             .post("/admin/user/edit", {
@@ -17,10 +14,8 @@ $(function () {
             .then ((response) => {
                 console.log("response:", response);
                 const result = response.data;
-                console.log("result:", result);
 
                 if (result.data) {
-                    console.log("User is updated!");
                     $(".member-status").blur();
                 } else alert("User update is failed!");
             })
