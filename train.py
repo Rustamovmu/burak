d67b41ecbe403717143b5524aa49d8fce68bb3a0
M.U <rustamovmuhammadumar2004@gmail.com>
2026-06-27 17:07:16 +0900

    feat: task Y is done

--- train.py
+++ train.py
@@ -1,26 +1,41 @@
+# TASK Y
+
+# Ikkala arrayda ham ishtirok etgan bir xil qiymatlarni qaytarsin.
+
+# Masalan: findIntersection([1, 2, 3], [3, 2, 0]) return [2, 3]
+
+def findIntersection(arr1, arr2):
+    result = []
+    for num in arr1:
+        if num in arr2:
+            result.append(num)
+    
+    return result
+
+print(findIntersection([1, 2, 3], [3, 2, 0]))
 # TASK X
 
 # Object tarkibida (nested bo'lsa ham) berilgan kalit necha marta takrorlanganini sanang.
 
 # Masalan: countOccurrences({model: "A", s: {model: "B"}}, "model") return 2
 
-def countOccurrences(obj, key):
-    count = 0
+# def countOccurrences(obj, key):
+#     count = 0
 
-    for k, v in obj.items():
-        if k == key:
-            count += 1
+#     for k, v in obj.items():
+#         if k == key:
+#             count += 1
 
-        if isinstance(v, dict):
-            count += countOccurrences(v, key)
+#         if isinstance(v, dict):
+#             count += countOccurrences(v, key)
 
-    return count
+#     return count
 
 
-print(countOccurrences(
-    {"model": "A", "s": {"model": "B"}},
-    "model"
-))
+# print(countOccurrences(
+#     {"model": "A", "s": {"model": "B"}},
+#     "model"
+# ))
 
 # TASK W
 
