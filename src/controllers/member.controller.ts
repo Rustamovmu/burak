ad5d4b2ea9ad16ef915de07611a5c183ad25cc15
M.U <rustamovmuhammadumar2004@gmail.com>
2026-07-06 20:28:03 +0900

    feat: develop updateMember business logic

--- src/controllers/member.controller.ts
+++ src/controllers/member.controller.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import { T } from "../libs/types/common";
 import MemberService from "../models/Member.service";
-import { Member, LoginInput, MemberInput, ExtendedRequest } from "../libs/types/member";
+import { Member, LoginInput, MemberInput, ExtendedRequest, MemberUpdateInput } from "../libs/types/member";
 import Errors, { HttpCode, Message } from "../libs/Errors";
 import AuthService from "../models/Auth.service";
 import { token } from "morgan";
@@ -78,8 +78,22 @@ memberController.getMemberDetail = async (req: ExtendedRequest, res: Response) =
         if (err instanceof Errors) res.status(err.code).json(err);
         else res.status(Errors.standart.code).json(Errors.standart);
     }
-}
+};
  
+memberController.updateMember = async(req: ExtendedRequest, res: Response) => {
+    try {
+        console.log("updateMember");
+        const input: MemberUpdateInput = req.body;
+        if(req.file) input.memberImage = req.file.path;
+        const result = await memberService.updateMember(req.member, input);
+
+        res.status(HttpCode.OK).json(result);
+    } catch (err) {
+        console.log("Error, updateMember:", err);
+        if (err instanceof Errors) res.status(err.code).json(err);
+        else res.status(Errors.standart.code).json(Errors.standart);
+    }
+};
 
 memberController.verifyAuth = async(req: ExtendedRequest, res: Response, next: NextFunction) => {
     try {
--- src/models/Auth.service.ts
+++ src/models/Auth.service.ts
@@ -31,12 +31,16 @@ class AuthService {
 
 
     public async checkAuth(token: string): Promise<Member>{
-        const result: Member = (await Jwt.verify(
-            token,
-            this.secretToken
-        )) as Member;
-        console.log(`-----[AUTH] memberNick: ${result.memberNick}------`);
-        return result;
+        try {
+            const result: Member = Jwt.verify(
+                token,
+                this.secretToken
+            ) as Member;
+            console.log(`-----[AUTH] memberNick: ${result.memberNick}------`);
+            return result;
+        } catch (err) {
+            throw new Errors(HttpCode.UNAUTHORIZED, Message.NOT_AUTHORIZED);
+        }
     }    
 }
 
